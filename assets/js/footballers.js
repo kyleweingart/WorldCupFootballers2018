4ed450180e186bcd9946bb30284162dffd52f0b3
Kyle Weingart <kyle.weingart@gmail.com>
2018-03-11 23:48:28 -0400

    updates to .js and .css

--- assets/js/footballers.js
+++ assets/js/footballers.js
@@ -18,37 +18,9 @@ $(document).ready(function () {
 
   // FUNCTIONS
   // ======================================================================================================
-  // function displayFootballerGifs() {
+  // function displayFootballerGifs() - when time create function for click event. 
 
-  //   var footballer = $(this).attr("footballer-name");
-  //   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-  //       footballer + "&api_key=" + APIkey + "&limit=10";
-
-
-  //   $.ajax({
-  //     url: queryURL,
-  //     method: "GET"
-  //   }).then(function(response) {
-  //     // there are a few URLs returned - which one is the best to use?
-  //     for (var i = 0; i <= response.data.length; i++) {
-  //       console.log(response.data[i].url);
-  //       var imageURL = response.data[i].url;
-  //       console.log(imageURL);
-  //       var footballerGIF = $("<img>");
-  //       footballerGIF.attr = ("src", imageURL);
-  //       footballerGIF.attr = ("alt", "footballer GIF");
-  //       $("#gif-view").append(footballerGIF);
-
-
-
-
-
-
-
-
-
-
-  function renderButtons() {
+   function renderButtons() {
 
     // Deleting the footballer buttons prior to adding new footballer buttons
     // (this is necessary otherwise we will have repeat buttons)
@@ -59,17 +31,18 @@ $(document).ready(function () {
     // Looping through the array of footballers
     for (var i = 0; i < footballers.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array.
-      // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+      // Then dynamically generating buttons for each footballer in the array.
+      
       var a = $("<button>");
       // Adding a class
       a.addClass("footballer");
       a.addClass("btn");
-      a.addClass("btn-primary");
+      a.addClass("btn-dark");
+      a.addClass("btn-kw");
     
-      // Adding a data-attribute with a value of the movie at index i
+      // Adding a data-attribute with a value of the footballer at index i
       a.attr("footballer-name", footballers[i]);
-      // Providing the button's text with a value of the movie at index i
+      // Providing the button's text with a value of the footballer at index i
       a.text(footballers[i]);
       // Adding the button to the HTML
       $("#footballers-view").append(a);
@@ -87,11 +60,13 @@ $(document).ready(function () {
   // Function for displaying the footballer gifs 
   // Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
   // $(document).on("click", ".footballer", displayFootballerGifs);
+  
+  
   $(".footballer").on("click", function () {
       $("#gif-view").empty();
       var footballer = $(this).attr("footballer-name");
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        footballer + "&api_key=" + APIkey + "&limit=10";
+        footballer + "&api_key=" + APIkey + "&limit=12";
 
 
       $.ajax({
@@ -102,20 +77,19 @@ $(document).ready(function () {
         .then(function(response) {
             
             var results = (response.data);
-
-            console.log(results);
            
-
-            // there are a few URLs returned - which one is the best to use?
             for (var i = 0; i <= results.length; i++) {
               console.log(results[i].images.fixed_height.url);
               var imageURL = results[i].images.fixed_height.url;
               console.log(imageURL);
               var footballerDiv = $("<div>");
+              footballerDiv.addClass("gif");
               var rating = $("<p>").text("Rating: " + results[i].rating);
+              rating.addClass("gif-heading")
               var footballerGIF = $("<img>");
               footballerGIF.attr("src", results[i].images.fixed_height.url);
               footballerGIF.attr("alt", "footballer GIF");
+              footballerGIF.addClass("gif-size");
               footballerDiv.append(rating);
               footballerDiv.append(footballerGIF)
               $("#gif-view").append(footballerDiv);
